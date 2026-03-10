81c2760b5699635e4659a39e0ee1b28355a4234d
andrewputraaaa <aaronandrewaqilaputra@gmail.com>
2026-03-10 09:31:05 +0700

    Add files via upload

---
+++ Invalidinput.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kuispbo236;
+
+/**
+ *
+ * @author Lab Informatika
+ */
+public class Invalidinput extends Exception{
+    public Invalidinput(){
+        super();
+    }
+    
+    public Invalidinput(string massage){
+        super(massage);
+    }
+    public Invalidinput(Throwable cause){
+        super(cause);
+    }
+    
+}
---
+++ Mahasiswa.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kuispbo236;
+
+/**
+ *
+ * @author Lab Informatika
+ */
+public abstract class Mahasiswa {
+   public  String nama;
+   
+   public Mahasiswa(String nama){
+       this.nama = nama;
+   }
+   
+   public abstract void tampilkanData();
+     
+   
+           
+    
+}
